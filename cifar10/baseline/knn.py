1f5023650b5bc3e6b624c322dd40cd01fae1274e
azraelkuan <azraelkuan@gmail.com>
2017-12-25 15:56:20 +0800

    update knn

--- cifar10/baseline/knn.py
+++ cifar10/baseline/knn.py
@@ -67,52 +67,5 @@ print('Got %d / %d correct => accuracy: %f' % (num_correct, len(Y_test), accurac
 
 with open('model/knn.pickle', 'wb') as f:
     pickle.dump(final_classifier, f)
-# # for cross validation
-#
-# num_folds = 5
-# k_choices = [1, 3, 5, 8, 10, 12, 15, 20, 50, 100]
-#
-#
-# range_split = np.array_split(range(x_train.shape[0]), num_folds)
-# y_train_folds = [y_train[range_split[i]] for i in range(num_folds)]
-# x_train_folds = [x_train[range_split[i]] for i in range(num_folds)]
-#
-# k_to_accuracies = {}
-#
-# for k in k_choices:
-#     for fold in range(num_folds): # This fold will be omitted.
-#         # Creating validation data and temp training data
-#         validation_x_test = x_train_folds[fold]
-#         validation_y_test = y_train_folds[fold]
-#         temp_x_train = np.concatenate(x_train_folds[:fold] + x_train_folds[fold + 1:])
-#         temp_y_train = np.concatenate(y_train_folds[:fold] + y_train_folds[fold + 1:])
-#
-#         # Initializing a class
-#         test_classifier = KNeighborsClassifier(n_neighbors=k)
-#         test_classifier.fit(temp_x_train, temp_y_train)
-#
-#         # Computing the distance
-#         temp_y_test_pred = test_classifier.predict(validation_x_test)
-#
-#         # Checking accuracies
-#         num_correct = np.sum(temp_y_test_pred == validation_y_test)
-#         num_test = validation_x_test.shape[0]
-#         accuracy = float(num_correct) / num_test
-#         k_to_accuracies[k] = k_to_accuracies.get(k,[]) + [accuracy]
-#
-# for k in sorted(k_to_accuracies):
-#     for accuracy in k_to_accuracies[k]:
-#         print("k = %d, accuracy = %f" % (k, accuracy))
-#
-# accuracies_mean = np.array([np.mean(v) for k, v in sorted(k_to_accuracies.items())])
-#
-# best_k = k_choices[np.argmax(accuracies_mean)]
-#
-# classifier = KNeighborsClassifier(n_neighbors=best_k)
-# classifier.fit(X_train, Y_train)
-# Y_test_pred = classifier.predict(X_test)
-#
-# num_correct = np.sum(Y_test_pred == Y_test)
-# accuracy = float(num_correct) / X_test.shape[0]
-# print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
+
 
