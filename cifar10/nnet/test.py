3ec63b7742c8a572f985074c6238ed7fabeea6f3
azraelkuan <azraelkuan@gmail.com>
2018-01-09 10:22:56 +0800

    add test

--- cifar10/nnet/test.py
+++ cifar10/nnet/test.py
@@ -3,14 +3,13 @@ import torchvision.transforms as transforms
 import os
 from torch.utils.data import Dataset, DataLoader
 from torch.autograd import Variable
-from skimage import io
-from skimage.transform import resize
-from tqdm import tqdm
-
+from PIL import Image 
+import numpy as np
 
 
 TEST_DATA_ROOT = "../data/testData/"
 use_cuda = torch.cuda.is_available()
+res = transforms.Resize((32, 32))
 
 
 def load_data():
@@ -32,7 +31,9 @@ class ImageData(Dataset):
         return len(self.file_paths)
 
     def __getitem__(self, idx):
-        image = io.imread(self.file_paths[idx])
+        image = Image.open(self.file_paths[idx])
+        resized_image = res(image)
+        image = np.array(resized_image)
         # image = resize(image, (32, 32), mode='reflect')
         file_name = self.file_paths[idx].split('/')[-1]
         if self.transform:
@@ -45,27 +46,26 @@ def test():
 
     file_paths = load_data()
     image_dataset = ImageData(file_paths, transform=transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize((32, 32)),
-        transforms.ToTensor()
-    ]))
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        ]))
     image_dataloader = DataLoader(image_dataset, num_workers=1, batch_size=1, shuffle=False)
 
-    model = torch.load('pkls/resnet50.pkl')
+    model = torch.load('pkls/cnn.pkl')
     net = model['model']
     acc = model['acc']
     print("acc: {}".format(acc))
     if use_cuda:
         net = net.cuda()
-
-    for data, file_name in tqdm(image_dataloader):
+    net.eval()
+    for data, file_name in image_dataloader:
         data = Variable(data)
         if use_cuda:
             data = data.cuda()
         predict = net(data)
         _, index = torch.max(predict.cpu().data, 1)
         test_txt.write("{} {}\n".format(file_name[0], index[0]))
-
+    print("Done!")
 
 if __name__ == '__main__':
     test()
